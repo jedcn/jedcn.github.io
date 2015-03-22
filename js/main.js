5516a2b1c5a9aaac51888c5566af237e81270e31
Jed Northridge <git@jedcn.com>
2015-03-22 08:03:06 -0400

    [test] What is window.location?

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var url = "https://api.github.com/users/jedcn";
+
+console.log(url);
+
+console.log(window.location);
+
+// $.ajax({
+//   url: url,
+// }).done(function(userInfo, result, jqXHR) {
+//   console.dir(userInfo);
+// });
